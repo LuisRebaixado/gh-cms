0facec71cf4cfe3a89d3e7d52040e33c9990a07b
Luis Felipe Manfroni <luis.manfroni@hotmail.com>
2022-10-31 20:26:08 -0300

    add commit logic

--- apps/test-gh-cms/src/main.ts
+++ apps/test-gh-cms/src/main.ts
@@ -1,5 +1,29 @@
-import { getOctokit, repoTest } from "github-cms";
+import { getOctokit, commitFiles } from "github-cms";
 
 const client = getOctokit(process.env["GH_TOKEN"]!);
-
-repoTest(client, "vercel", "turbo");
+const jsonContent = JSON.stringify({
+  test: 123,
+  abc: 123,
+  one: "two,three",
+  k: [1, 2, 3],
+  e: false
+});
+commitFiles({
+  octokit: client,
+  authorEmail: "lrebaixado@izishop.com",
+  authorName: "lrebaixado",
+  branch: "main",
+  message: "add blabla2",
+  owner: "LuisRebaixado",
+  repo: "sturdy-invention",
+  files: [
+    {
+      path: "test.json",
+      content: jsonContent,
+      mode: "100644",
+      type: "commit"
+    }
+  ]
+})
+  .then(() => console.log("sucess"))
+  .catch(console.log);
--- packages/github-cms/src/main.ts
+++ packages/github-cms/src/main.ts
@@ -3,12 +3,53 @@ import { Octokit } from "octokit";
 export const getOctokit = (token: string) => {
   return new Octokit({ auth: token });
 };
-export const repoTest = async (octokit: Octokit, owner: string, repo: string) => {
+export const getLatestCommit = async (octokit: Octokit, owner: string, repo: string) => {
   const commits = await octokit.rest.repos.listCommits({
     owner,
     repo
   });
-  commits.data.forEach((d) => {
-    console.log(d.commit.author);
+  return commits.data[0];
+};
+interface File {
+  path: string;
+  mode: "100644" | "100755" | "040000" | "160000" | "120000";
+  type: "commit" | "tree" | "blob";
+  sha?: string | null;
+  content: string;
+}
+export const commitFiles = async (parameters: {
+  octokit: Octokit;
+  owner: string;
+  repo: string;
+  branch: string;
+  message: string;
+  files: File[];
+  authorName: string;
+  authorEmail: string;
+}) => {
+  const latestCommit = await getLatestCommit(parameters.octokit, parameters.owner, parameters.repo);
+  const currentTree = await parameters.octokit.rest.git.createTree({
+    owner: parameters.owner,
+    repo: parameters.repo,
+    tree: parameters.files,
+    base_tree: latestCommit.sha
+  });
+  const currentCommit = await parameters.octokit.rest.git.createCommit({
+    owner: parameters.owner,
+    repo: parameters.repo,
+    message: parameters.message,
+    tree: currentTree.data.sha,
+    parents: [latestCommit.sha],
+    author: {
+      name: parameters.authorName,
+      email: parameters.authorEmail,
+      date: new Date().toISOString()
+    }
+  });
+  await parameters.octokit.rest.git.updateRef({
+    owner: parameters.owner,
+    repo: parameters.repo,
+    ref: `heads/${parameters.branch}`,
+    sha: currentCommit.data.sha
   });
 };
